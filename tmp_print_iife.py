de70070e656adfeafaea07887419374deead6745
山本　竜三 <ryu@yamakun.net>
2025-10-16 20:07:53 +0900

    Remove unused WMS features: tmp files, generate_wms_based_html_page method, and pyqgis_independent_renderer.py

--- tmp_print_iife.py
+++
@@ -1,6 +0,0 @@
-from qmap_permalink.qmap_webmap_generator import QMapWebMapGenerator
-wg = QMapWebMapGenerator()
-html = wg.generate_wms_based_html_page({'x':0,'y':0,'scale':10000,'themes':['A','B','C'],'bookmarks':[{'name':'b1','x':0,'y':0}]},800,600,8089)
-start = html.find('(function(){')
-end = html.find('})();', start)
-print(html[start:end+4])
