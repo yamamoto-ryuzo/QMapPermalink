d17712efd29cd21ab42b8f1e8d13ec46bc774dad
山本　竜三 <ryu@yamakun.net>
2025-10-24 22:05:31 +0900

    WMTS導入

--- tmp_test_open.py
+++
@@ -1,3 +0,0 @@
-from qmap_permalink import qmap_maplibre as g
-p = g.open_maplibre_from_permalink('35.6895,139.6917,12')
-print('Returned path:', p)
--- tmp_test_parse.py
+++
@@ -1,8 +0,0 @@
-from qmap_permalink import qmap_maplibre as g
-cases=[
- 'https://example.com/?lat=35.6895&lon=139.6917&zoom=12',
- '35.6895,139.6917,12',
- '@35.6895,139.6917,12z'
-]
-for c in cases:
-    print(c, '->', g._parse_permalink(c))
