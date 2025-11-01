5dbc440ea254d6bbb5c37b974a0f49f1c9165639
山本　竜三 <ryu@yamakun.net>
2025-11-02 06:29:56 +0900

    リポジトリクリーニング

--- run_open_maplibre.py
+++
@@ -1,7 +0,0 @@
-from qmap_permalink.qmap_maplibre import open_maplibre_from_permalink
-
-permalink = 'http://localhost:8089/qgis-map?x=-21673.330850&y=-9560.153850&scale=3738.5&crs=EPSG:6677&rotation=95.00'
-typename = '橋脚旗揚げ'
-
-path = open_maplibre_from_permalink(permalink, wfs_typename=typename)
-print('HTML path:', path)
